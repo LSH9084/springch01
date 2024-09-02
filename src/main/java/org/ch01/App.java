134e49be1560a3bcc7cb5ab139c8fea67b519152
lsh9084 <sanghun1101088@gmail.com>
2024-09-02 12:37:13 +0900

    .

---
+++ src/main/java/org/ch01/App.java
@@ -0,0 +1,16 @@
+package org.ch01;
+
+/**
+ * 날짜 : 2024.09.02
+ * 이름 : 이상훈
+ * 내용 : 스프링 기초
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+        System.out.println("Hello Spring");
+    }
+}
